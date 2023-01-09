7da36d58ef5f42e55880e129a447e6be290cd139
Juan Covarrubias <me@juan.fi>
2023-01-09 17:29:03 +0200

    FrontEnd: Change BaseUrl to proxy

--- frontend/src/services/drones.ts
+++ frontend/src/services/drones.ts
@@ -2,17 +2,8 @@ import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 
 const dronesBaseUrl = `${BASE_URL}/drones`;
-const config = {
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-  }
-};
-
 const getDrones = async (): Promise<undefined> => {
-  const request = axios.get(dronesBaseUrl, config);
-  (await request).headers.set
-  console.log('moi');
+  const request = await axios.get(dronesBaseUrl);
   console.log(request);
   return undefined;
 };
--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://assignments.reaktor.com/birdnest';
+export const BASE_URL = 'http://localhost:3001/api';
 export const NFZ_RADIUS_METERS = 100;
