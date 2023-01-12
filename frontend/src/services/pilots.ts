877f3f6da3c7d26ec5df8ab2c936b16be56168d3
Juan Covarrubias <me@juan.fi>
2023-01-12 16:42:55 +0200

    FrontEnd: Pilot Service getPilotOfDrone function

--- frontend/src/services/pilots.ts
+++ frontend/src/services/pilots.ts
@@ -1,7 +1,20 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { BASE_URL } from '../utils/constants';
+import { Pilot } from '../utils/interfaces';
+import { parsePilot } from '../utils/validation';
 
-const getPilotOfDrone = (droneSerialNumber: string): undefined => {
-  console.log(droneSerialNumber);
-  return undefined;
+const pilotsApiUrl = `${BASE_URL}/pilots`;
+
+const getPilotOfDrone = async (droneSerialNumber: string): Promise<Pilot | undefined> => {
+  try {
+    const apiResponse: AxiosResponse<unknown, unknown> = await axios.get(`${pilotsApiUrl}/${droneSerialNumber}`);
+    const user = parsePilot(apiResponse.data);
+    return user;
+  } catch (error) {
+    if (error instanceof AxiosError && error.response && error.response.status === 404) {
+      return undefined;
+    }
+  }
 };
 
 export default {
