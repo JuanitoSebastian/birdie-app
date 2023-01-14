2503aeb8c5fca92d8e2c7367e4280e2afe87b156
Juan Covarrubias <me@juan.fi>
2023-01-14 14:46:40 +0200

    FrontEnd: DronesView grid responsive to screen width

--- frontend/src/components/DronesView.tsx
+++ frontend/src/components/DronesView.tsx
@@ -10,7 +10,7 @@ const DronesView = (props: DronesViewProps) => {
   if (!props.drones) return (<></>);
 
   return (
-    <div className='grid grid-cols-4 gap-4'>
+    <div className='grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-4'>
       {props.drones.map((drone) => (
             <DroneCard key={drone.serialNumber} drone={drone} />
           ))}
