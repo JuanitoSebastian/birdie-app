1aedbb04c113840f75a2cadd288b90e5e6a787fa
Juan Covarrubias <me@juan.fi>
2023-01-14 12:09:34 +0200

    FrontEnd: DroneCard displayes unkown pilots

--- frontend/src/components/DroneCard.tsx
+++ frontend/src/components/DroneCard.tsx
@@ -46,15 +46,23 @@ const DroneCard = (props: DroneCardProps) => {
       </div>
     )
     : (
-      <tr>
-        <td>
-          <div className='flex flex-col items-start justify-center text-sm'>
-            <p className='font-bold'>Unkown pilot</p>
+      <div className="card card-compact bg-base-100 shadow-xl">
+        <div className='card-body'>
+          <p className='card-title'>Unkown Pilot</p>
+          <div className='flex flex-row gap-2 items-center'>
+            <Icon type='email' />
+            <p>-</p>
           </div>
-        </td>
-        <td>{props.drone.serialNumber}</td>
-        <td>{closestDistanceToNest}</td>
-      </tr>
+          <div className='flex flex-row gap-2 items-center'>
+            <Icon type='phone' />
+            <p>-</p>
+          </div>
+          <div className='flex flex-col gap-0 items-end'>
+            <p className='font-mono text-xs'>Closest Violation</p>
+            <p className='font-bold text-lg'>{closestDistanceToNest} m</p>
+          </div>
+        </div>
+      </div>
     );
 
 };
