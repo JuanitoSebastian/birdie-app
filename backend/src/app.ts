278012ce71575411e08cc644291244e22438ba97
Juan Covarrubias <me@juan.fi>
2023-01-15 13:13:36 +0200

    BackEnd: Fix JsDoc

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 const baseUrl = 'https://assignments.reaktor.com/birdnest';
 
 /**
- * Returns drones within 500m^2 of nest
+ * Returns drones within 100m^2 of nest
  */
 app.get('/drones', ((_request: Request, response: Response, _next: NextFunction) => {
   response.send(DroneService.getDrones());
